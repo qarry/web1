81e69aa4925442c358d5fbb4c6549de176d4598a
root <root@aillas.lyonesse>
2013-05-07 14:01:37 +0200

    web1 on aillas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web1::Application.config.secret_token = '975636dde38ad8d7a4cd55cdc220c608031e15dcfc19165bb72d42a762f19ff84dcc224091c71cf69431117d6b42cd7706a26b3f3a3d8fb76d1ae0d1aa71bd89'
